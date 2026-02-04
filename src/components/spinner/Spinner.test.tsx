f14692f52d71c25acd86eee7100b319b2009c09d
likithayadav_g <minutommy1919@gmail.com>
2026-02-04 14:39:51 +0530

    refactor(spinner): simplify renderSpinner function and remove redundant tests

--- src/components/spinner/Spinner.test.tsx
+++ src/components/spinner/Spinner.test.tsx
@@ -3,16 +3,12 @@ import userEvent from "@testing-library/user-event";
 import { describe, it, expect } from "vitest";
 import { Spinner, type SpinnerProps } from "./Spinner";
 
-type RenderOptions = {
-	additionalProps?: Partial<SpinnerProps>;
-};
-
 const defaultProps: SpinnerProps = {
 	size: "md",
 	className: "",
 };
 
-const renderSpinner = ({ additionalProps }: RenderOptions = {}) => {
+const renderSpinner = (additionalProps?: Partial<SpinnerProps>) => {
 	const user = userEvent.setup();
 
 	render(<Spinner {...defaultProps} {...additionalProps} />);
@@ -28,35 +24,5 @@ describe("Spinner", () => {
 		const { spinner } = renderSpinner();
 
 		expect(spinner).toBeInTheDocument();
-	});
-
-	it("should render medium size spinner by default", () => {
-		const { spinner } = renderSpinner();
-
-		expect(spinner).toHaveClass("w-8 h-8");
-	});
-
-	it("should render small size spinner when size is sm", () => {
-		const { spinner } = renderSpinner({
-			additionalProps: { size: "sm" },
-		});
-
-		expect(spinner).toHaveClass("w-4 h-4");
-	});
-
-	it("should render large size spinner when size is lg", () => {
-		const { spinner } = renderSpinner({
-			additionalProps: { size: "lg" },
-		});
-
-		expect(spinner).toHaveClass("w-12 h-12");
-	});
-
-	it("should apply custom className when provided", () => {
-		const { spinner } = renderSpinner({
-			additionalProps: { className: "text-red-500" },
-		});
-
-		expect(spinner).toHaveClass("text-red-500");
 	});
 });
